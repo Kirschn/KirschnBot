33c46c1eb7a3a5e52a87f6a2c76d18e47683d60e
Kirschnkiller <kirschnkiller@googlemail.com>
2015-12-28 23:06:15 +0100

    New Login Page

--- webinterface/login.php
+++ webinterface/login.php
@@ -40,7 +40,7 @@ if (isset($_SESSION["kbot_logon"])) {
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>AdminLTE 2 | Log in</title>
+    <title>KirschnBot | Log in</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.5 -->
