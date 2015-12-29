a733c23cb6cdf5dccec7b38e49d3cc8017a2a5f3
Kirschn <github@kirschnkiller.de>
2015-12-29 18:52:26 +0100

    Added ressources to main pool

---
+++ webinterface/function/sqlinit.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mcwmc
+ * Date: 26.12.2015
+ * Time: 15:01
+ */
+$sqlconnection = mysqli_connect("localhost", "kirschnbot", "Jnq9jD38KPnF8fDT", "kirschnbot");;
