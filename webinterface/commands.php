c89f65caa48d2fdb42fa3d3f42bac316aa2513e6
Kirschnkiller <kirschnkiller@googlemail.com>
2015-12-29 23:33:27 +0100

    typo

--- webinterface/commands.php
+++ webinterface/commands.php
@@ -447,16 +447,8 @@ desired effect
             </ul><!-- /.control-sidebar-menu -->
         </div>
     </aside><!-- /.control-sidebar -->
-    <!-- Add the sidebar's background. This div must be placed
-         immediately after the control sidebar -->
     <div class="control-sidebar-bg"></div>
 </div><!-- ./wrapper -->
 
-<!-- REQUIRED JS SCRIPTS -->B
-
-<!-- Optionally, you can add Slimscroll and FastClick plugins.
-     Both of these plugins are recommended to enhance the
-     user experience. Slimscroll is required when using the
-     fixed layout. -->
 </body>
 </html>
