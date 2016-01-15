a58a03834cec5676dcd51eaa6d2116ffb99a1dda
Kirschnkiller <kirschnkiller@googlemail.com>
2016-01-16 00:37:07 +0100

    Copyright Update

--- webinterface/include/footer.php
+++ webinterface/include/footer.php
@@ -4,5 +4,5 @@
         Script runtime: <?php echo round(microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"], 4); ?> Seconds | <a href="http://github.com/Kirschn/KirschnBot">KirschnBot V2.0.1.8 </a>
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2015 <a href="http://kirschn.de">Kirschn</a></strong>
+    <strong>Copyright &copy; 2016 <a href="http://kirschn.de">Kirschn</a></strong>
 </footer>
