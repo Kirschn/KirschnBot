0006b1e2ecbedd7ac6b3be8cef1db05b2dfad242
Kirschnkiller <kirschnkiller@googlemail.com>
2016-02-21 02:28:39 +0100

    Added base for User Switching

--- webinterface/include/switchbot.php
+++ webinterface/include/switchbot.php
@@ -10,14 +10,21 @@
                                 <div class="modal-body" id="successcontent">
                                     <select>
                                         <?php
-                                    $manageingpermissions = mysqli_fetch_array($sqlconnection, mysqli_query($sqlconnection, "SELECT channel FROM canmanage WHERE name='".$_SESSION["kbot_realusername"]."';"));
-                                        echo var_dump($manageingpermissions);
-                                        $manageingpermissions[] = $_SESSION["kbot_realusername"];
+                                    $manageingpermissions = mysqli_fetch_assoc(mysqli_query($sqlconnection, "SELECT channel FROM canmanage WHERE name='".$_SESSION["kbot_realusername"]."'"));
+                                    $manageingpermissions[] = $_SESSION["kbot_realusername"];
                                     foreach ($manageingpermissions as $currentchan) {
                                         if ($currentchan == $_SESSION["kbot_managementbot"]) {
-                                            ?><option selected><?php echo $currentchan; ?></option><?php
+                                            ?>
+                                                <option selected>
+                                                    <?php echo $currentchan; ?>
+                                                </option>
+                                            <?php
                                         } else {
-                                            ?><option><?php echo $currentchan; ?></option><?php
+                                            ?>
+                                                <option>
+                                                    <?php echo $currentchan; ?>
+                                                </option>
+                                            <?php
                                         }
                                     }
                                     ?>
