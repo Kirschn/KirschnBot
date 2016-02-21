04ccec8df4e2b0399fdab75ac2848bb64834b75c
Kirschnkiller <kirschnkiller@googlemail.com>
2016-02-21 22:04:46 +0100

    Added base for User Switching

--- webinterface/include/switchbot.php
+++ webinterface/include/switchbot.php
@@ -1,4 +1,15 @@
-
+<?php
+$manageingpermissions = mysqli_fetch_assoc(mysqli_query($sqlconnection, "SELECT channel FROM canmanage WHERE name='".$_SESSION["kbot_realusername"]."'"));
+if (isset($_POST["channel"]) && isset($_POST["token"])){
+    session_start();
+    $manageingpermissions[] = $_SESSION["kbot_realusername"];
+    if (in_array($_POST["channel"], $manageingpermissions) && $_POST["token"] == $_SESSION["onetimetoken"])  {
+        $_SESSION["kbot_managementbot"] = $_POST["channel"];
+        echo "ok";
+        die();
+    }
+}
+?>
 
 <div class="modal fade" id="switchbot" tabindex="-1" role="dialog" aria-labelledby="success">
                         <div class="modal-dialog" role="document">
@@ -8,10 +19,10 @@
                                     <h4 class="modal-title" id="successname">Switch Bot</h4>
                                 </div>
                                 <div class="modal-body" id="successcontent">
-                                    <select>
+                                    <select id="channelselector">
                                         <?php
-                                    $manageingpermissions = mysqli_fetch_assoc(mysqli_query($sqlconnection, "SELECT channel FROM canmanage WHERE name='".$_SESSION["kbot_realusername"]."'"));
-                                    $manageingpermissions[] = $_SESSION["kbot_realusername"];
+                                        $manageingpermissions[] = $_SESSION["kbot_realusername"];
+
                                     foreach ($manageingpermissions as $currentchan) {
                                         if ($currentchan == $_SESSION["kbot_managementbot"]) {
                                             ?>
@@ -31,6 +42,19 @@
 
 
                                     </select>
+                                    <script>
+                                        $("#channelselector").on('change', '#channelselector', function() {
+                                            $.post("include/switchbot.php", {
+                                                onetimetoken: "<?php echo $_SESSION["onetimetoken"]; ?>",
+                                                channel: $("#channelselector").value;
+                                            }).done(function (data) {
+                                                if (data=="ok") {
+                                                    location.reload();
+                                                }
+                                                });
+                                            });
+                                        });
+                                    </script>
                                 </div>
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
