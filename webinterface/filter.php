b060acab5ec7b8abeaf4d4cd94143e89205623d2
Kirschn <github@kirschn.de>
2016-03-01 23:55:12 +0100

    Initial Filter Commit

--- webinterface/filter.php
+++ webinterface/filter.php
@@ -253,15 +253,26 @@ desired effect
         <section class="content">
             <div class="box box-info">
                 <div class="box-header with-border">
-                    <h3 class="box-title">Confirmed Users</h3>
+                    <h3 class="box-title">Linkfilter</h3>
                     <div class="box-tools pull-right">
                         <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
                     </div>
                 </div><!-- /.box-header -->
-                <div class="box-body" id="confirmedusertable">
+                <div class="box-body" id="linkfiltersettings">
                     Loading...
                 </div>
             </div>
+    <div class="box box-info">
+        <div class="box-header with-border">
+            <h3 class="box-title">Blacklist filterung</h3>
+            <div class="box-tools pull-right">
+                <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+            </div>
+        </div><!-- /.box-header -->
+        <div class="box-body" id="blacklistfilter">
+            Loading...
+        </div>
+    </div>
             <?php if ($canmanage) {
             ?>
 
