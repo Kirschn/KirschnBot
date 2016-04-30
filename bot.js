ec6cc670f9c0b3fe26be3b9cdc2416fdef8ffbdb
Kirschn <github@kirschn.de>
2016-05-01 01:13:03 +0200

    Added Timer

--- bot.js
+++ bot.js
@@ -15,7 +15,8 @@ fs = require("fs"), // Config File lesen
     spawn = require('child_process').spawn, // Fork Prozesse geniereren
     mail = require("sendmail")(), // Komische Mails senden wenn alles explodiert
     linkregex = /[\d\sa-zA-Z][.](com|to|ly|aaa|aarp|abb|abbott|abogado|ac|academy|accenture|accountant|accountants|aco|active|actor|ad|adac|ads|adult|ae|aeg|aero|af|afl|ag|agency|ai|aig|airforce|airtel|al|alibaba|alipay|allfinanz|alsace|am|amica|amsterdam|analytics|android|ao|apartments|app|apple|aq|aquarelle|ar|aramco|archi|army|arpa|arte|as|asia|associates|at|attorney|au|auction|audi|audio|author|auto|autos|aw|ax|axa|az|azure|ba|baidu|band|bank|bar|barcelona|barclaycard|barclays|bargains|bauhaus|bayern|bb|bbc|bbva|bcn|bd|be|beats|beer|bentley|berlin|best|bet|bf|bg|bh|bharti|bi|bible|bid|bike|bing|bingo|bio|biz|bj|black|blackfriday|bloomberg|blue|bm|bms|bmw|bn|bnl|bnpparibas|bo|boats|boehringer|bom|bond|boo|book|boots|bosch|bostik|bot|boutique|br|bradesco|bridgestone|broadway|broker|brother|brussels|bs|bt|budapest|bugatti|build|builders|business|buy|buzz|bv|bw|by|bz|bzh|ca|cab|cafe|cal|call|camera|camp|cancerresearch|canon|capetown|capital|car|caravan|cards|care|career|careers|cars|cartier|casa|cash|casino|cat|catering|cba|cbn|cc|cd|ceb|center|ceo|cern|cf|cfa|cfd|cg|ch|chanel|channel|chat|cheap|chloe|christmas|chrome|church|ci|cipriani|circle|cisco|citic|city|cityeats|ck|cl|claims|cleaning|click|clinic|clinique|clothing|cloud|club|clubmed|cm|cn|co|coach|codes|coffee|college|cologne|com|commbank|community|company|compare|computer|comsec|condos|construction|consulting|contact|contractors|cooking|cool|coop|corsica|country|coupon|coupons|courses|cr|credit|creditcard|creditunion|cricket|crown|crs|cruises|csc|cu|cuisinella|cv|cw|cx|cy|cymru|cyou|cz|dabur|dad|dance|date|dating|datsun|day|dclk|de|dealer|deals|degree|delivery|dell|deloitte|delta|democrat|dental|dentist|desi|design|dev|diamonds|diet|digital|direct|directory|discount|dj|dk|dm|dnp|do|docs|dog|doha|domains|download|drive|dubai|durban|dvag|dz|earth|eat|ec|edeka|edu|education|ee|eg|email|emerck|energy|engineer|engineering|enterprises|epson|equipment|er|erni|es|esq|estate|et|eu|eurovision|eus|events|everbank|exchange|expert|exposed|express|fage|fail|fairwinds|faith|family|fan|fans|farm|fashion|fast|feedback|ferrero|fi|film|final|finance|financial|firestone|firmdale|fish|fishing|fit|fitness|fj|fk|flickr|flights|florist|flowers|flsmidth|fly|fm|fo|foo|football|ford|forex|forsale|forum|foundation|fox|fr|fresenius|frl|frogans|frontier|fund|furniture|futbol|fyi|ga|gal|gallery|gallup|game|garden|gb|gbiz|gd|gdn|ge|gea|gent|genting|gf|gg|ggee|gh|gi|gift|gifts|gives|giving|gl|glass|gle|global|globo|gm|gmail|gmo|gmx|gn|gold|goldpoint|golf|goo|goog|google|gop|got|gov|gp|gq|gr|grainger|graphics|gratis|green|gripe|group|gs|gt|gu|gucci|guge|guide|guitars|guru|gw|gy|hamburg|hangout|haus|hdfcbank|health|healthcare|help|helsinki|here|hermes|hiphop|hitachi|hiv|hk|hm|hn|hockey|holdings|holiday|homedepot|homes|honda|horse|host|hosting|hoteles|hotmail|house|how|hr|hsbc|ht|hu|hyundai|ibm|icbc|ice|icu|id|ie|ifm|iinet|il|im|immo|immobilien|in|industries|infiniti|info|ing|ink|institute|insurance|insure|int|international|investments|io|ipiranga|iq|ir|irish|is|iselect|ist|istanbul|it|itau|iwc|jaguar|java|jcb|je|jetzt|jewelry|jlc|jll|jm|jmp|jo|jobs|joburg|jot|joy|jp|jprs|juegos|kaufen|kddi|ke|kfh|kg|kh|ki|kia|kim|kinder|kitchen|kiwi|km|kn|koeln|komatsu|kp|kpn|kr|krd|kred|kw|ky|kyoto|kz|la|lacaixa|lamborghini|lamer|lancaster|land|landrover|lanxess|lasalle|lat|latrobe|law|lawyer|lb|lc|lds|lease|leclerc|legal|lexus|lgbt|li|liaison|lidl|life|lifeinsurance|lifestyle|lighting|like|limited|limo|lincoln|linde|link|live|living|lixil|lk|loan|loans|lol|london|lotte|lotto|love|lr|ls|lt|ltd|ltda|lu|lupin|luxe|luxury|lv|ly|ma|madrid|maif|maison|makeup|man|management|mango|market|marketing|markets|marriott|mba|mc|md|me|med|media|meet|melbourne|meme|memorial|men|menu|meo|mg|mh|miami|microsoft|mil|mini|mk|ml|mm|mma|mn|mo|mobi|mobily|moda|moe|moi|mom|monash|money|montblanc|mormon|mortgage|moscow|motorcycles|mov|movie|movistar|mp|mq|mr|ms|mt|mtn|mtpc|mtr|mu|museum|mutuelle|mv|mw|mx|my|mz|na|nadex|nagoya|name|natura|navy|nc|ne|nec|net|netbank|network|neustar|new|news|nexus|nf|ng|ngo|nhk|ni|nico|nikon|ninja|nissan|nl|no|nokia|norton|nowruz|np|nr|nra|nrw|ntt|nu|nyc|nz|obi|office|okinawa|om|omega|one|ong|onl|online|ooo|oracle|orange|org|organic|origins|osaka|otsuka|ovh|pa|page|pamperedchef|panerai|paris|pars|partners|parts|party|pe|pet|pf|pg|ph|pharmacy|philips|photo|photography|photos|physio|piaget|pics|pictet|pictures|pid|pin|ping|pink|pizza|pk|pl|place|play|playstation|plumbing|plus|pm|pn|pohl|poker|porn|post|pr|praxi|press|pro|prod|productions|prof|promo|properties|property|protection|ps|pt|pub|pw|pwc|py|qa|qpon|quebec|quest|racing|re|read|realtor|realty|recipes|red|redstone|redumbrella|rehab|reise|reisen|reit|ren|rent|rentals|repair|report|republican|rest|restaurant|review|reviews|rexroth|rich|ricoh|rio|rip|ro|rocher|rocks|rodeo|room|rs|rsvp|ru|ruhr|run|rw|rwe|ryukyu|sa|saarland|safe|safety|sakura|sale|salon|samsung|sandvik|sandvikcoromant|sanofi|sap|sapo|sarl|sas|saxo|sb|sbs|sc|sca|scb|schaeffler|schmidt|scholarships|school|schule|schwarz|science|scor|scot|sd|se|seat|security|seek|select|sener|services|seven|sew|sex|sexy|sfr|sg|sh|sharp|shell|shia|shiksha|shoes|show|shriram|si|singles|site|sj|sk|ski|skin|sky|skype|sl|sm|smile|sn|sncf|so|soccer|social|softbank|software|sohu|solar|solutions|song|sony|soy|space|spiegel|spot|spreadbetting|sr|srl|st|stada|star|starhub|statefarm|statoil|stc|stcgroup|stockholm|storage|store|studio|study|style|su|sucks|supplies|supply|support|surf|surgery|suzuki|sv|swatch|swiss|sx|sy|sydney|symantec|systems|sz|tab|taipei|taobao|tatamotors|tatar|tattoo|tax|taxi|tc|tci|td|team|tech|technology|tel|telecity|telefonica|temasek|tennis|tf|tg|th|thd|theater|theatre|tickets|tienda|tiffany|tips|tires|tirol|tj|tk|tl|tm|tmall|tn|to|today|tokyo|tools|top|toray|toshiba|tours|town|toyota|toys|tr|trade|trading|training|travel|travelers|travelersinsurance|trust|trv|tt|tube|tui|tunes|tushu|tv|tvs|tw|tz|ua|ubs|ug|uk|unicom|university|uno|uol|us|uy|uz|va|vacations|vana|vc|ve|vegas|ventures|verisign|versicherung|vet|vg|vi|viajes|video|viking|villas|vin|vip|virgin|vision|vista|vistaprint|viva|vlaanderen|vn|vodka|volkswagen|vote|voting|voto|voyage|vu|wales|walter|wang|wanggou|watch|watches|weather|weatherchannel|webcam|weber|website|wed|wedding|weir|wf|whoswho|wien|wiki|williamhill|win|windows|wine|wme|wolterskluwer|work|works|world|ws|wtc|wtf|xbox|xerox|xin|xn|xperia|xxx|xyz|yachts|yahoo|yamaxun|yandex|ye|yodobashi|yoga|yokohama|youtube|yt|za|zara|zero|zip|zm|zone|zuerich|zw)/ig; // Linkerkennung
-timer = []; // Timer IDs aus SetInterval ablegen
+timers = [],
+    activitytimer = {};// Timer IDs aus SetInterval ablegen
 var concat = require('concat-stream'); // Wird für Strawpoll gebraucht
 var JSONStream = require('JSONStream'); // Ebenfalls für Strawpoll
 var strawpoll = require('strawpoll'); // Backend für !strawpoll
@@ -236,7 +237,7 @@ setTimeout(function () {
             refreshbotcommands(channel); // Anstoßen von Command Refresh
             refreshbotusers(channel); // Refresh von Festen Nutzerleveln
             // SQL Config auslesen. Kein SELECT * verwendet da es bei Peaks einen kleinen Performancevorteil bietet
-            sqlconnection.query("SELECT linkfilter, blacklistfilter, linktolength, blacklisttolength, linktotext, blacklisttotext, maxtoul, silentto, silentlinkto, silentblacklistto, id FROM botconfig WHERE channel=" + mysql.escape(channel) + ";", function (err, results) {
+            sqlconnection.query("SELECT linkfilter, quotes, blacklistfilter, linktolength, blacklisttolength, linktotext, blacklisttotext, maxtoul, silentto, silentlinkto, silentblacklistto, id FROM botconfig WHERE channel=" + mysql.escape(channel) + ";", function (err, results) {
                 if (err == null) {
                     // Results aus SQL übertragen in Lokalen Variablen Cache. Yaay 300000 gleiche Zeilen (._.
                     activebots["config"][channel] = {};
@@ -252,6 +253,9 @@ setTimeout(function () {
                     activebots["config"][channel].silentblacklistto = (results[0].silentblacklistto == "0") ? false : true;
                     activebots["config"][channel].permit = "";
                     activebots["config"][channel].id = results[0].id;
+                    activebots["config"][channel].quotelevel = results[0].minquoteuserlevel;
+                    activebots["config"][channel].quoteswitch = (results[0].quotes == "0") ? false : true;
+                    activitytimer[channel] = 0;
                 }
             });
             // Linkwhitelist in den Cache schreiben für Linkfilter
@@ -730,7 +734,8 @@ setTimeout(function () {
                             var url = String(current[0].split(")")[0]).replace("http(", "").replace(",", "");
                             if (url.length < 500) {
                                 request(url, function (error, response, body) {
-                                    if (response == undefined) {
+                                    console.log("Error: " + error + " Response: " + response + " Body: " + body);
+                                    if (body == undefined) {
                                         callback(handlemessage.replace(url, "Invalid Syntax"));
                                     } else {
                                         if (!error && response.statusCode == 200) {
@@ -850,6 +855,9 @@ setTimeout(function () {
 
         function parsecom(nick, channel, text, funcret, tos) {
             util.log("MESSAGE " + channel + " => " + nick + " => " + text);
+            console.log(activitytimer);
+            activitytimer[channel] += 1;
+            console.log(activitytimer);
             if (activebots["config"][channel] !== undefined && tos && !friendlymode) {
                 if (activebots["config"][channel].linkfilter) {
                     if (linkregex.test(text.toLowerCase())) {
@@ -1002,20 +1010,26 @@ setTimeout(function () {
                         }
                     });
                 }
-                if (splitmessagelowercase[0] == "!zitat" || splitmessagelowercase[0] == "!quote") {
-                    if (splitmessagelowercase[1] !== undefined) {
-                        // Zitatsname ist Wort 1!
-                        var sql = "SELECT text FROM quotes WHERE name=" + mysql.escape(splitmessagelowercase[1]) + " AND (channel=" + mysql.escape(channel) + " OR channel=\"global\") ORDER BY RAND() LIMIT 1;";
-                    } else {
-                        var sql = "SELECT text FROM quotes WHERE (channel=" + mysql.escape(channel) + " OR channel=\"global\") ORDER BY RAND() LIMIT 1;";
-                    }
-                    sqlconnection.query(sql, function (err, results) {
-                        if (results[0] !== undefined) {
-                            funcret(channel, results[0]["text"])
-                        } else {
-                            funcret(channel, "Quote does not exist");
+                if ((splitmessagelowercase[0] == "!zitat" || splitmessagelowercase[0] == "!quote") && activebots["config"][channel].quoteswitch) {
+                    getuserlevel(nick, channel, function (level) {
+                        if (level <= activebots["config"][channel].quotelevel) {
+                            if (splitmessagelowercase[1] !== undefined) {
+                                // Zitatsname ist Wort 1!
+                                var sql = "SELECT text FROM quotes WHERE name=" + mysql.escape(splitmessagelowercase[1]) + " AND (channel=" + mysql.escape(channel) + " OR channel=\"global\") ORDER BY RAND() LIMIT 1;";
+                            } else {
+                                var sql = "SELECT text FROM quotes WHERE (channel=" + mysql.escape(channel) + " OR channel=\"global\") ORDER BY RAND() LIMIT 1;";
+                            }
+                            sqlconnection.query(sql, function (err, results) {
+                                if (results[0] !== undefined) {
+                                    funcret(channel, results[0]["text"])
+                                } else {
+                                    funcret(channel, "Quote does not exist");
+                                }
+                            });
                         }
+
                     });
+
                 }
                 if (splitmessagelowercase[0] == "!addquote" && splitmessagelowercase[1] !== undefined && splitmessagelowercase[2] !== undefined) {
                     getuserlevel(nick, channel, function (level) {
@@ -1287,8 +1301,38 @@ setTimeout(function () {
                 clientaws.say(retchannel, text)
             }, true)
         });
+         var timerloop = setInterval(function() {
+            var time = new Date;
+            var currentTime = Date.parse(time)/1000;
 
+                 var sql = "SELECT name, text, channel, timerinterval, linex FROM timer WHERE active=" + mysql.escape(true) +";";
+                 sqlconnection.query(sql, function(err, results) {
+                     if (err) throw err;
+                     results.forEach(function(current) {
+                        if (currentTime / (current["timerinterval"]*60) == Math.floor(currentTime / (current["timerinterval"]*60))) {
+                             if (activebots["config"][current["channel"]] !== undefined && current["linex"] <= activitytimer[current["channel"]]) {
+                                 if (timers[current["channel"] + "#" + current["name"]] == true || timers[current["channel"] + "#" + current["name"]] == undefined) {
+                                     client.say(current["channel"], current["text"]);
+                                     timers[current["channel"] + "#" + current["name"]] = false;
+                                 } else {
+                                 }
 
+                             }
+                         } else {
+                             timers[current["channel"] + "#" + current["name"]] = true;
+                         }
+                     });
+
+                 });
+
+            if (currentTime / 300 == Math.floor(currentTime / 300)) {
+                for (var index in activitytimer) {
+                    if (activitytimer.hasOwnProperty(index)) {
+                        activitytimer[index] = 0;
+                    }
+                }
+            }
+        }, 1000);
 // TODOCRAWLER
 
         function getinteractions() {
