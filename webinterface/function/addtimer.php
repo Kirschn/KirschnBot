a2d7ba1acec7aa12b8049ef5b42a8db7d53583cf
Kirschn <github@kirschn.de>
2016-05-01 20:25:48 +0200

    Fixed JS

--- webinterface/function/addtimer.php
+++ webinterface/function/addtimer.php
@@ -7,7 +7,7 @@ if (isset($_POST["token"])) {
         if (isset($_POST["commandname"]) && isset($_POST["commandtext"]) && isset($_POST["interval"]) && isset($_POST["lines"])) {
 
             if ($_POST["commandname"] !== "" && $_POST["commandtext"] !== "" && $_POST["interval"] !== ""&& $_POST["lines"] !== "") {
-                $sql = 'INSERT INTO timer (name, text, timerinterval, linex, channel) VALUES ("' . mysqli_real_escape_string($sqlconnection, $_POST["commandname"]) . '", "' . mysqli_real_escape_string($sqlconnection, $_POST["commandtext"]) . '" , "' . mysqli_real_escape_string($sqlconnection, $_POST["interval"]) . '" , "' . mysqli_real_escape_string($sqlconnection, $_POST["lines"]) . '", "' . $_SESSION["kbot_managementbot"] . '");';
+                $sql = 'INSERT INTO timer (name, text, timerinterval, linex, channel) VALUES ("' . mysqli_real_escape_string($sqlconnection, $_POST["commandname"]) . '", "' . mysqli_real_escape_string($sqlconnection, $_POST["commandtext"]) . '" , "' . mysqli_real_escape_string($sqlconnection, $_POST["interval"]) . '" , "' . mysqli_real_escape_string($sqlconnection, $_POST["lines"]) . '", "#' . $_SESSION["kbot_managementbot"] . '");';
                 mysqli_query($sqlconnection, $sql);
                 echo "Operation complete.";
             } else {
